56fab6d0e33fc76cf79136174f0e8031faa57bc5
AndreM <am@welectron.com>
2025-09-16 15:32:59 +0200

    script to pick Raspberry PIs from boxes

---
+++ pick_up_raspberrypi.py
@@ -0,0 +1,275 @@
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from xarm_msgs.msg import RobotMsg
+from geometry_msgs.msg import Pose, Point, PointStamped
+from xarm_msgs.srv import MoveCartesian, MoveJoint, VacuumGripperCtrl, GetInt16
+from tf2_ros import Buffer, TransformListener
+from tf2_geometry_msgs import do_transform_point
+import tf2_ros
+from array import array
+import time
+class InitialPoseClient(Node):
+    def __init__(self):
+        super().__init__('initialpose_ufactory_client')
+        self.cli = self.create_client(MoveJoint, '/ufactory/set_servo_angle')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = MoveJoint.Request()
+
+    def send_request(self, j1, j2, j3, j4, j5, j6):
+        self.req.angles = (float(j1), float(j2), float(j3), float(j4), float(j5), float(j6))
+        self.req.speed = 0.50
+        self.req.mvtime = 0.0
+        self.req.wait = False
+        self.req.timeout = -1.0
+        self.req.radius = -1.0
+        self.req.relative = False
+        self.req.acc = 10.0
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
+class RobotStateSubscriber(Node):
+    def __init__(self):
+        super().__init__('robot_state_subscriber')
+        self.subscription = self.create_subscription(
+            RobotMsg,              
+            '/ufactory/robot_states',
+            self.state_callback,
+            10
+        )
+        self.latest_state = None
+        self.get_logger().info("Subscribed from /ufactory/robot_states")
+
+    def state_callback(self, msg):
+        self.latest_state = msg
+        self.get_logger().debug(f"State received: {msg}")
+
+    def get_latest_state(self):
+        return self.latest_state
+    
+class VacuumGripperUFactoryClient(Node):
+    def __init__(self):
+        super().__init__('vacuum_gripper_ufactory_client')
+        self.cli = self.create_client(VacuumGripperCtrl, '/ufactory/set_vacuum_gripper')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = VacuumGripperCtrl.Request()
+        self.running = True
+
+    def send_request(self, is_active):
+        self.req.on = is_active
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+
+class GetVacuumGripperState(Node):
+    def __init__(self):
+        super().__init__('get_vacuum_gripper_state')
+        self.cli = self.create_client(GetInt16, '/ufactory/get_vacuum_gripper')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = GetInt16.Request()
+        self.running = True
+
+    def send_request(self):
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        resp = self.future.result()
+        return resp.data
+
+class PoseSubscriberClient(Node):
+    def __init__(self):
+        super().__init__('raspberry_pose_subscriber_client')
+
+        # Client to move the arm in Cartesian coordinates
+        self.cli = self.create_client(MoveCartesian, '/ufactory/set_position')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for ufactory/set_position service...')
+        self.req = MoveCartesian.Request()
+        self.move_pose_client = InitialPoseClient()
+        self.ufactory_control_vacuum_gripper = VacuumGripperUFactoryClient()
+        self.get_state_vacuum_gripper = GetVacuumGripperState()
+        self.robot_state_sub = RobotStateSubscriber()
+
+        self.already_executed = False  #Flag for executed detection
+
+
+         # TF2 listener
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
+        # Subscription to the topic that a Pose publishes
+        self.subscription = self.create_subscription(
+            Point,
+            'detected_object_coordinates',
+            self.pose_callback,
+            10
+        )
+
+        self.get_logger().info('Node started. Waiting for pose at /detected_object_coordinates...')
+
+    def pose_callback(self, msg):
+
+        if self.already_executed:
+            self.get_logger().info("Move was executed for this detection, Msg from topic is ignored.")
+            return
+
+        self.already_executed = True  # Flag for executed detection
+
+        try:    
+            for j in range(3):
+                for i in range(4):
+                    #Subscribe pose first detected raspberry pi (z-component)!!!!
+                    x_matrix= -24 + (62*i) #hard code
+                    y_matrix= -171 - (93*j) #hard code
+                    z_matrix= 184 #subscribe z-component
+                    x = msg.x
+                    y = msg.y
+                    z = msg.z
+
+                    #Move robot arm to initial robot state
+                    self.move_pose_client.send_request(0.07, 0.19, 1.45, 3.16, -1.30, 1.84)
+
+                    #Move robot arm to Etikettendrucker
+                    self.move_pose_client.send_request(0.07, 0.57, 0.99, 3.18, -0.46, 1.81)
+
+                    #Move robot arm over box
+                    self.move_pose_client.send_request(-1.36, 0.19, 1.43, 3.12, -1.2, 1.73)
+
+                    #Move robot to Raspberry PI Pose
+                    self.send_request(x_matrix, y_matrix, z_matrix, 80)
+
+                    #Move robot arm over box
+                    self.move_pose_client.send_request(-1.36, 0.19, 1.43, 3.12, -1.2, 1.73)
+
+                    #Move robot arm to initial robot state
+                    self.move_pose_client.send_request(0.07, 0.19, 1.45, 3.16, -1.30, 1.84)
+
+                    #Move robot arm to goal position (up)
+                    self.move_pose_client.send_request(0.58, 0.54, 1.62, 3.18, -1.11, 2.33)
+
+                    #Move robot arm to goal position (down)
+                    self.move_pose_client.send_request(0.58, 1.06, 1.50, 3.23, -0.42, 2.28)
+
+                    #Move robot arm to goal position (up)
+                    self.move_pose_client.send_request(0.58, 0.54, 1.62, 3.18, -1.11, 2.33)
+
+                    #Move robot arm to initial robot state
+                    self.move_pose_client.send_request(0.07, 0.19, 1.45, 3.16, -1.30, 1.84)
+
+                    #Move robot arm to initial robot state
+                    #self.move_pose_client.send_request(0.07, 0.19, 1.45, 3.16, -1.30, 1.84)
+
+            """
+
+            #Move to detected Pose + z offset(2cm)
+            z_offset = z + 20
+            self.get_logger().info(f'Move to detected Pose + z offset(5m): x={x:.2f}, y={y:.2f}, z={z_offset:.2f}')
+            self.send_request(x, y, z_offset, 100)
+
+            #Start Vacuum Gripper
+            self.ufactory_control_vacuum_gripper.send_request(True)
+
+            
+            #Slow linear move to get raspberry pi
+            while True:
+                state_gripper = self.get_state_vacuum_gripper.send_request()
+
+                if state_gripper == 1:
+                    self.get_logger().info("Vacuum gripper ha detectado succión. Saliendo del bucle.")
+                    break
+
+                robot_state = self.robot_state_sub.get_latest_state()
+                if robot_state is not None:
+                    #pose = robot_state.pose
+                    pose_list = robot_state.pose
+                    x = pose_list[0]
+                    y = pose_list[1]
+                    z = pose_list[2] 
+                    self.get_logger().info(f"Actual pose: x={x}, y={y}, z={z}")
+
+                    #Calculate new pose going down 5mm
+                    new_z_pose = z - 5.0
+
+                    #Send new move with less velocity
+                    self.send_request(x, y, new_z_pose, 20)
+                else:
+                    self.get_logger().warn("robot_state is None, waiting message...")
+                    time.sleep(0.1)
+                
+            #Linear move up
+            self.send_request(x, y, z_offset, 100)
+
+            #Move to goal Pose (up)
+            self.move_pose_client.send_request(-1.61, -0.02, 1.52, 0.02, 1.57, 1.60)
+
+            #Move to goal Pose (down)
+            self.move_pose_client.send_request(-1.60, 1.60, 1.33, -0.04, -0.24, 1.64)
+
+            #Stop Vacuum Gripper
+            self.ufactory_control_vacuum_gripper.send_request(False)
+
+            #Move robot arm to goal pose (up)(again)
+            self.move_pose_client.send_request(-1.61, -0.02, 1.52, 0.02, 1.57, 1.60)
+
+            #Move robot arm to initial position
+            self.move_pose_client.send_request(0.15, -0.02, 1.52, 0.02, 1.57, 1.60)
+            """
+
+        except Exception as e:
+            self.get_logger().warn(f'Error transforming pose: {str(e)}')
+
+    def send_request(self, x, y, z, velocity):
+        # Send request to cartesian move
+        self.req.pose = array('f', [float(x), float(y), float(z),3.1 , 0.01, 0.06])
+        self.get_logger().info(f'Sending pose: [{x}, {y}, {z},3.1 , 0.01, 0.06')
+        self.req.speed = float(velocity) #100
+        self.req.acc = 0.0 
+        self.req.mvtime = 0.0
+        self.req.wait = True
+        self.req.timeout = -1.0
+        self.req.radius = -1.0
+
+        self.get_logger().info('Calling service...')
+        future = self.cli.call_async(self.req)
+
+        rclpy.spin_until_future_complete(self, future, timeout_sec=1.0)
+        #future.add_done_callback(callback)
+        if future.done():
+            result = future.result()
+            if result is not None:
+                self.get_logger().info('Service answered.')
+                self.get_logger().info(f'Movement sent successfully. Result: {result}')
+            else:
+                self.get_logger().error('Future result is None.')
+        else:
+            self.get_logger().error('Timeout waiting for set_position service response.')
+
+        if future.result() is not None:
+            self.get_logger().info('Movement sent successfully.')
+        else:
+            self.get_logger().error('Error calling the service.')
+
+def callback(future):
+    result = future.result()
+  
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PoseSubscriberClient()
+
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+    executor.add_node(node.robot_state_sub)
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        node.robot_state_sub.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
